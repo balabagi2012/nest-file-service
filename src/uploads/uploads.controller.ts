934f2dda2c3fc838df32ba062f54034f57a56599
LeoZhang <balabagi2012@gmail.com>
2024-03-20 16:08:57 +0800

    feat: upload and get file

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,16 +1,41 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { UploadsService } from './uploads.service';
+
 @Controller('uploads')
 export class UploadsController {
   constructor(private readonly uploadsService: UploadsService) {}
-  @Get()
-  getFile(): string {
-    return;
+
+  @Get(':imgPath')
+  getFile(@Param('imgPath') imgPath, @Res() res): string {
+    return res.sendFile(imgPath, { root: './uploads' });
   }
 
   @Post()
-  uploadFile(): string {
-    return;
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const fileExtName = file.originalname.split('.')[1];
+          const randomName = Date.now().toString();
+          callback(null, `${randomName}.${fileExtName}`);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return { message: 'File uploaded successfully!', file: `/${file.path}` };
   }
 
   @Delete()
